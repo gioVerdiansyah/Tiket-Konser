ce0b3fc2801c928a76bc1fac5659a363c4d17440
putra03pratama <putra09prtama@gmail.com>
2023-10-08 00:32:43 +0700

    responsif history

--- resources/views/layouts/nav_profile.blade.php
+++ resources/views/layouts/nav_profile.blade.php
@@ -1,47 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Tambahkan link CSS Bootstrap di sini -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <title>Profile User</title>
-        <style>
-            .profile-image {
-                width: 180px
-            }
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Tambahkan link CSS Bootstrap di sini -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <title>Profile User</title>
+    <style>
+        .profile-image {
+            width: 180px
+        }
 
-            #pp {
-                display: none;
-            }
-        </style>
-    </head>
+        #pp {
+            display: none;
+        }
 
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <!-- Ganti kelas "bg-body-tertiary" dengan "navbar-light bg-light" -->
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span> <!-- Tambahkan ikon Hamburger Menu -->
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="{{ route('index') }}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route('profileUser') }}">Profil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('history') }}">History</a>
-                        </li>
-                        <li class="nav-item d-flex align-items-center">
-                            <form method="POST" id="form-logout" action="{{ route('logout') }}"
-                                onsubmit="
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- Ganti kelas "bg-body-tertiary" dengan "navbar-light bg-light" -->
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span> <!-- Tambahkan ikon Hamburger Menu -->
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="{{ route('index') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('profileUser') }}">Profil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('history') }}">History</a>
+                    </li>
+                    <li class="nav-item ">
+                        <form method="POST" id="form-logout" action="{{ route('logout') }}"
+                            onsubmit="
                             event.preventDefault();
                             Swal.fire({
                           title: `Tunggu dulu!`,
@@ -53,32 +54,32 @@
                           }
                           })
                         ">
-                                @csrf
-                                <button type="submit" class="dropdown-item nav-link">Logout</button>
-                            </form>
-                        </li>
-                    </ul>
-                </div>
+                            @csrf
+                            <button type="submit" class="dropdown-item nav-link">Logout</button>
+                        </form>
+                    </li>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-        @yield('content')
+    @yield('content')
 
-        <!-- Tambahkan skrip Bootstrap JS di sini (setelah jQuery) -->
-        @if (session('message'))
-            <script>
-                Swal.fire({
-                    icon: "{{ session('message')['icon'] ?? 'success' }}",
-                    title: "{{ session('message')['title'] }}",
-                    text: "{{ session('message')['text'] }}",
-                    timer: {{ session('message')['timer'] ?? 5000 }},
-                    allowOutsideClick: false,
-                    allowEscapeKey: false,
-                });
-            </script>
-        @endif
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    </body>
+    <!-- Tambahkan skrip Bootstrap JS di sini (setelah jQuery) -->
+    @if (session('message'))
+        <script>
+            Swal.fire({
+                icon: "{{ session('message')['icon'] ?? 'success' }}",
+                title: "{{ session('message')['title'] }}",
+                text: "{{ session('message')['text'] }}",
+                timer: {{ session('message')['timer'] ?? 5000 }},
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+            });
+        </script>
+    @endif
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
 
 </html>
